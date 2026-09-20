0d56bae9e1546c8c9da7e360a34a1ed5d8250802
pranay9-h <62534115+pranay9-h@users.noreply.github.com>
2026-09-20 17:32:48 +0530

    feat(env): add prod environment variables

---
+++ environments/prod/variables.tf
@@ -0,0 +1,45 @@
+variable "aws_region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "project_name" {
+  type    = string
+  default = "portfolio-eks"
+}
+
+variable "cluster_name" {
+  type    = string
+  default = "portfolio-prod-eks"
+}
+
+variable "cluster_version" {
+  description = "Optional EKS version. Leave null to let AWS choose a supported default."
+  type        = string
+  default     = null
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.20.0.0/16"
+}
+
+variable "availability_zones" {
+  type    = list(string)
+  default = ["ap-south-1a", "ap-south-1b"]
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.20.1.0/24", "10.20.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.20.11.0/24", "10.20.12.0/24"]
+}
+
+variable "node_instance_types" {
+  type    = list(string)
+  default = ["t3.medium"]
+}
